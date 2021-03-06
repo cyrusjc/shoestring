8db678c6354c68dfbb33ddd8c9923b04abd55dbb
cyrusjc <cyrus.cheung123@gmail.com>
2021-03-06 21:57:39 +0000

    finish tcp

--- C Server/tcp_server.c
+++ C Server/tcp_server.c
@@ -5,13 +5,14 @@
 #include <stdlib.h>
 #include <netinet/in.h>
 #include <string.h>
-
 #include <pthread.h>        // for multithreading
-
 #include <dirent.h>
 
 #define PORT 4000
 #define BACKLOG 10
+
+#define BUFSIZE 40000
+
 
 int createSocket(){
     int server_fd;
@@ -55,6 +56,104 @@ void listFiles(int socket){
     return;
 }
 
+void printFile(int socket){
+    
+    char buffer[BUFSIZE];
+    size_t bytes_read;
+    int msgsize = 0;
+    char actualpath[PATH_MAX+1];
+    
+    char *msg = "Which file would you like to read? ";
+    send(socket, msg, strlen(msg), 0);
+    
+    
+    recv(socket, buffer, sizeof(buffer),0);
+    printf("\nFile is:  %s\n", buffer);
+    
+    // checking for path validity
+    if (realpath(buffer, actualpath) == NULL){
+        
+        char error = 1;
+        send(socket, &error, 1,0); 
+        printf("Bad Path: %s\n", buffer);
+        return;
+    }
+  
+    send(socket, msg, strlen(msg), 0); 
+    
+
+    
+    FILE *fp = fopen(actualpath, "r");
+    if (fp == NULL){
+        printf(" can't open %s\n", buffer);
+        return;
+    } 
+
+    
+    while( (bytes_read = fread(buffer,1,BUFSIZE,fp))>0 ){
+        printf("sending %zu bytes\n", bytes_read);
+        send(socket,buffer,bytes_read,0);
+    }
+    
+    memset(buffer, 0, sizeof(buffer));
+    fflush(stdout);
+    fclose(fp);
+    
+    return;
+}
+
+void uploadFile (int socket){
+    int size = 10;
+    int finish;
+    char actualpath[PATH_MAX+1];
+    
+    char send_buffer[BUFSIZE], read_buffer[256];
+    
+    char *msg = "What would you like to download?";
+    send(socket, msg, strlen(msg), 1);
+    
+    memset(read_buffer, '\0', sizeof(read_buffer));
+    recv(socket, read_buffer, sizeof(read_buffer),0);
+    printf("\nFile is:  %s\n", read_buffer);
+    
+    // checking for path validity
+    if (realpath(read_buffer, actualpath) == NULL){
+        
+        size = 0;
+        send(socket, (void*)&size, sizeof(int),0); 
+        printf("Bad Path: %s\n", read_buffer);
+        return;
+    }
+    
+    FILE *fp = fopen(actualpath, "r");
+    if (fp == NULL){
+        printf("Can't open %s\n", read_buffer);
+        return;
+    } 
+
+    
+    fseek(fp,0,SEEK_END);
+    size = ftell(fp);
+    fseek(fp, 0, SEEK_SET);
+    send(socket, (void*)&size, sizeof(int), 0); 
+     
+    
+    while (!feof(fp)){
+        size = fread(send_buffer,1,sizeof(send_buffer)-1, fp);
+        
+        do{
+            finish = write(socket,send_buffer,size);
+        } while(finish < 0);
+        
+        printf("Packet Size: %i\n", size);
+        memset(send_buffer, '\0' ,sizeof(send_buffer));
+
+    }
+    return;
+    
+    
+}
+
 void * handler(void *p_socket){
     
         int socket = *((int*)p_socket);
@@ -62,32 +161,36 @@ void * handler(void *p_socket){
         int cont = 1;
         
         char buffer[30000] = {0};
-        char *hello = "What would you like to do?\n 1. List\n 0. Terminate";
+        char *hello = "What would you like to do?\n 1. List\n 2. Read File\n 3. Upload \n 0. Terminate";
         send(socket , hello , strlen(hello), 0);
         
         while(cont){
+            
             recv( socket , buffer, 30000, 0);
             printf("recieved: %s\n",buffer );
             switch(buffer[0])
             {
                 case '1': 
-                    printf("1\n");
                     //send(socket, "Sending Files", strlen("Sending Files"), 0);
                     listFiles(socket);
                     break;
+                case '2':
+                    printFile(socket);
+                    break;
+                case '3':
+                    uploadFile(socket);
+                    break;
                 case '0':
-                    printf("Terminating socket\n");
                     cont = 0;
                     break;
                 default:
-                    printf("Error wrong socket\n");
-                    send(socket, "Please enter another number from", 32,0 );
+                    send(socket, "Please enter another number: ", 32,0 );
                     break;
             }
-            buffer[0] = '\0';
+            memset(buffer, 0, sizeof(buffer));
         }
         
-
+        printf("Terminating socket\n");
         free(p_socket);
         close(socket);
         return NULL;
